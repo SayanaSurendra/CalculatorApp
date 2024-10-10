b98eb05dc89d8854933b7a5a9e574d8c51ed8c94
Sayana Surendran <sayanasurendran31@gmail.com>
2024-10-10 11:48:30 +0200

    refactor code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,57 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-     Calculator calculator= new Calculator();
-     calculator.cal();
+import org.example.Calculator;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // boolean isCalculate= true , allows user to continue the program
+        boolean isCalculate = true;
+        while (isCalculate) {
+            System.out.println("Enter first number:");
+            int num1=in.nextInt();
+            int num2=in.nextInt();
+            System.out.println("Enter operator choice: 1.Add 2.Subtract 3.Multiplication 4.Division");
+            int operator = in.nextInt();
+            switch (operator) {
+                case 1:
+                    System.out.println(Calculator.add(num1, num2));
+                    break;
+
+                case 2:
+                    System.out.println(Calculator.subtract(num1,num2));
+                    break;
+
+                case 3:
+                    System.out.println (Calculator.multiply(num1, num2));
+                    break;
+
+                case 4:
+                    System.out.println( Calculator.division(num1, num2));
+                    break;
+
+                default:
+                    System.out.println("Invalid Operator");
+                    break;
+            }
+            System.out.println("Enter  0 to Exit the program or 1 to continue");
+            int option = in.nextInt();
+            if (option == 0) {
+                System.out.println("Program Exited");
+                isCalculate = false;
+            }
+
+
+        }
     }
 }
+
+
+
+
+
+
