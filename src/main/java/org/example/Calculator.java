f776545b5264aa6a773c1b46637e3b8e4399a2c0
Sayana Surendran <sayanasurendran31@gmail.com>
2024-10-10 15:39:54 +0200

    multiple operands for addition/subtraction

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Calculator {
 
     public static int add(int num1, int num2) {
         //System.out.println(num1 + "+" + num2 + "=" + (num1 + num2));
-        return num1 + num2;
+        return num1 +num2;
     }
 
 
@@ -24,4 +26,58 @@ public class Calculator {
         // System.out.println(num1 + "/" + num2 + "=" + (num1 / num2));
         return num1 / num2;
     }
+
+    public static  int advancedAddition() {
+        Scanner in=new Scanner(System.in);
+        int  sum=0;
+
+        while(true){
+            System.out.println("Enter numbers(or enter + to calculate)");
+            String input= in.nextLine();
+            if(input.equals("+")){
+                break;
+            }
+            int num1= Integer.parseInt(input);
+            sum=Calculator.add(num1,sum);
+
+        }
+        return  sum;
+
+    }
+
+    public static  int advancedSubtract(){
+        Scanner in=new Scanner(System.in);
+        int  diff=0, num1=0;
+        while(true){
+            System.out.println("Enter numbers(or enter - to calculate)");
+            String input= in.nextLine();
+            if(input.equals("-")){
+                break;
+            }
+            if(diff == 0) {
+                num1 = Integer.parseInt(input);
+            }else{
+                num1=diff;
+                diff=Integer.parseInt(input);
+            }
+            diff=Calculator.subtract(num1,diff);
+
+        }
+        return  diff;
+    }
 }
+
+ /*if(input.contains("+")) {
+String  arr[]=input.split("\\+");
+        int sum=0,i;
+            for( i=0;i<arr.length;i++) {
+        if(i==0){
+sum=0;
+        }
+
+int num1= Integer.parseInt(arr[i]);
+sum=Calculator.add(num1,sum);
+
+            }
+                    System.out.println(sum);
+        }*/
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,40 +1,36 @@
 package org.example;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
+        int num1 = 0, num2 = 0;
+        Scanner in=new Scanner(System.in);
         // boolean isCalculate= true , allows user to continue the program
         boolean isCalculate = true;
         while (isCalculate) {
-            System.out.println("Enter first number:");
-            int num1=in.nextInt();
-            int num2=in.nextInt();
-            System.out.println("Enter operator choice: 1.Add 2.Subtract 3.Multiplication 4.Division");
-            int operator = in.nextInt();
-            switch (operator) {
-                case 1:
-                    System.out.println(Calculator.add(num1, num2));
-                    break;
-
-                case 2:
-                    System.out.println(Calculator.subtract(num1,num2));
-                    break;
-
-                case 3:
-                    System.out.println (Calculator.multiply(num1, num2));
-                    break;
-
-                case 4:
-                    System.out.println( Calculator.division(num1, num2));
-                    break;
-
-                default:
-                    System.out.println("Invalid Operator");
-                    break;
+            int operator= printOpertorChoice(in);
+            //to perform advanced addition and subtraction
+            if (operator==5 ||operator ==6){
+                performCalculation(operator,num1,num2);
             }
+            // to perform basic calculation
+            else{
+                System.out.println("Enter 2 numbers:");
+                try {
+                    num1 = in.nextInt();
+                    num2 = in.nextInt();
+
+                } catch (InputMismatchException e) {
+                    System.out.println("Input is not an Integer");
+                } catch (Exception e) {
+                    System.out.println("Exception:" + e.getMessage());
+                }
+                performCalculation(operator, num1, num2);
+            }
+
             System.out.println("Enter  0 to Exit the program or 1 to continue");
             int option = in.nextInt();
             if (option == 0) {
@@ -42,9 +38,50 @@ public class Main {
                 isCalculate = false;
             }
 
-
         }
     }
+
+
+    public static int printOpertorChoice( Scanner in) {
+        System.out.println("Enter operator choice: 1.Add 2.Subtract 3.Multiplication 4.Division 5.Advanced Addition 6.Advanced Subtract");
+        int op = in.nextInt();
+        return op;
+    }
+
+
+    private static void performCalculation(int operator,int num1,int num2) {
+        switch (operator) {
+            case 1:
+                System.out.println(Calculator.add(num1, num2));
+                break;
+
+            case 2:
+                System.out.println(Calculator.subtract(num1, num2));
+                break;
+
+            case 3:
+                System.out.println(Calculator.multiply(num1, num2));
+                break;
+
+            case 4:
+                System.out.println(Calculator.division(num1, num2));
+                break;
+
+            case 5:
+                System.out.println(Calculator.advancedAddition());
+                break;
+
+            case 6:
+                System.out.println(Calculator.advancedSubtract());
+                break;
+
+            default:
+                System.out.println("Invalid Operator");
+                break;
+        }
+    }
+
+
 }
 
 
